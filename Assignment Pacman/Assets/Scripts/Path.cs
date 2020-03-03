53c82a9d3bb96b70805eb21510beb8bb8384c8f1
Frank <fmarinellicollege@gmail.com>
2020-03-03 15:31:49 -0500

    Starting Assignment

---
+++ Assignment Pacman/Assets/Scripts/Path.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+	public static Vector3[] myPath;
+	public static GameObject[] Checkpoints;
+	public GameObject Goal;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
